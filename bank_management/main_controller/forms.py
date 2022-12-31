d7744417b95352e0b0c84ae43930e3a69a0d8396
Soheil Hoseini <soheil.hosseini78@gmail.com>
2022-12-31 23:02:58 +0330

    update signup form

--- bank_management/main_controller/forms.py
+++ bank_management/main_controller/forms.py
@@ -1,18 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
 from .models import Profile
 
 
 class SignUpForm(UserCreationForm):
-    country = forms.CharField(max_length=100)
 
     class Meta:
         model = User
-        fields = ('username', 'country', 'password1', 'password2')
+        fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2')
 
     def save(self, commit=True):
         user = super().save(commit=commit)
-        Profile.objects.create(user=user, country=self.data['country'])
+        Profile.objects.create(user=user)
         return user
--- bank_management/main_controller/models.py
+++ bank_management/main_controller/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=50, blank=True, null=True)
-    country = models.CharField(max_length=100)
 
 
 class PersonalInformation(models.Model):
