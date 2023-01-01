d50baef193334bd3ac54713bbfb5a428138ab75a
Soheil Hoseini <soheil.hosseini78@gmail.com>
2023-01-01 12:25:10 +0330

    cors allow

--- bank_management/bank_management/settings.py
+++ bank_management/bank_management/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_controller',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+ALLOWED_HOSTS=['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'bank_management.urls'
 
